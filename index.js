e3308d5a087eea2d4f7d7a7e5393f217dee2fb9d
Moringa School <samuel.kamotho@student.moringaschool.com>
2021-07-21 14:07:11 +0300

    change the display to none

---
+++ index.js
@@ -0,0 +1,42 @@
+jQuery('h1').click(function(){
+    alert("This is a heading tag")
+  });
+
+  jQuery('p').click(function(){
+    alert("This is a paragraph")
+  });
+
+  jQuery('img').click(function(){
+    alert("This is an image")
+  });
+  jQuery("h1").click(function() {
+    alert("This is a header.");
+    alert("I told you, THIS IS A HEADER!");
+  });
+  jQuery(document).ready(function() {
+    jQuery("h1").click(function() {
+      alert("This is a header.");
+    });
+  
+    jQuery("p").click(function() {
+      alert("This is a paragraph.");
+    });
+  
+    jQuery("img").click(function() {
+      alert("This is an image.");
+    });
+  });
+  $(document).ready(function() {
+    $("h1").click(function() {
+      alert("This is a header.");
+    });
+  
+    $("p").click(function() {
+      alert("This is a paragraph.");
+    });
+  
+    $("img").click(function() {
+      alert("This is an image.");
+    });
+  });
+  
